14bc3452e35b1ed5b5d40f2b17775e2ac59a165c
João Batista <jbsouzasantos1999@gmail.com>
2022-05-06 00:05:31 -0300

    Modal using portals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
+import Modal from "./Modal";
+import React, { useState } from "react";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className={classes.container}>
+        <header className={classes.header}>
+          <button className={classes.button} onClick={handleOpenModal}>
+            Open Modal
+          </button>
+        </header>
+        <div className={classes.content}></div>
+      </div>
+      <Modal open={isOpen} onClose={handleCloseModal}>
+        <h4>I used a React Portal to render this component propertly.</h4>
+        <p>(You can also click outside to close this modal!)</p>
+      </Modal>
+    </React.Fragment>
   );
 }
 
